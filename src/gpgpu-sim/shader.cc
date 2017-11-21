b0a328060118ee633c61804beca7555f9d5eb596
unknown <samgh1230@github.com>
2017-11-21 19:29:12 +0800

    update

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -2026,7 +2026,7 @@ void gpgpu_sim::shader_print_cache_stats( FILE *fout ) const{
     }
 
     // L1D
-    FILE* f=fopen("blk_stats.txt","a");
+    FILE* f=fopen("blk_stat.txt","a");
     if(!m_shader_config->m_L1D_config.disabled()){
         total_css.clear();
         css.clear();
@@ -2045,6 +2045,7 @@ void gpgpu_sim::shader_print_cache_stats( FILE *fout ) const{
             fprintf(fout, "\tL1D_total_cache_miss_rate = %.4lf\n", (double)total_css.misses / (double)total_css.accesses);
             fprintf(f,"%.4f\n",(double)total_css.misses/(double)total_css.accesses);
         }
+        else fprintf(f,"0.0\n");
         fprintf(fout, "\tL1D_total_cache_pending_hits = %u\n", total_css.pending_hits);
         fprintf(fout, "\tL1D_total_cache_reservation_fails = %u\n", total_css.res_fails);
         total_css.print_port_stats(fout, "\tL1D_cache"); 
