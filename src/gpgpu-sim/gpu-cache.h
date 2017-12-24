c867c5e6eebc14f7a267454fa4f75b51d5c4b3e8
unknown <samgh1230@github.com>
2017-12-25 00:39:11 +0800

    update

--- src/gpgpu-sim/gpu-cache.h
+++ src/gpgpu-sim/gpu-cache.h
@@ -1161,7 +1161,7 @@ protected:
     		unsigned time, bool &do_miss, std::list<cache_event> &events, bool read_only, bool wa);
     /// Read miss handler. Check MSHR hit or MSHR available
     void send_read_request(new_addr_type addr, new_addr_type block_addr, unsigned cache_index, mem_fetch *mf,
-    		unsigned time, bool &do_miss, bool &wb, cache_block_t &evicted, std::list<cache_event> &events, bool read_only, bool wa);
+    		unsigned time, bool &do_miss, bool &wb, std::vector<cache_block_t> &evicted, std::list<cache_event> &events, bool read_only, bool wa);
 
     /// Sub-class containing all metadata for port bandwidth management 
     class bandwidth_management 
